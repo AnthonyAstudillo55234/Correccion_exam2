55d02560df691881121e8bbdd94e1df4b56c083a
Mateor13 <mateo.torres@epn.edu.ec>
2024-06-20 08:29:18 -0500

    cambio-correcion-Anthony-Mateo

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,34 @@ public class Main {
         ServicioExtra cliente2= new ServicioExtra("Carlos","1736105951",23,"Guayaquil","5678","15/06/2024","16/06/2024",8,"Guayaquil","Ambato",25.25,"Televisión,internet ",2,true,5.00,"Comida");
         Normal cliente3 = new Normal("María", "1726195208", 27, "Cuenca", "9012", "16/06/2024", "17/06/2024", 3, "Cuenca", "Quito", 18.75, "pasillo", 1, false, 4.50);
         ServicioExtra cliente4 = new ServicioExtra("Pedro", "1736105952", 31, "Ambato", "3456", "17/06/2024", "18/06/2024", 6, "Ambato", "Guayaquil", 22.50, "Televisión", 2, true, 4.75, "Comida");
-        ServicioExtra cliente6 = new ServicioExtra("Andrés", "1736105953", 29, "Machala", "2468", "19/06/2024", "20/06/2024", 7, "Machala", "Quito", 24.00, "Televisión,internet", 2, false, 5.50, "Comida");
+        ServicioExtra cliente5 = new ServicioExtra("Andrés", "1736105953", 29, "Machala", "2468", "19/06/2024", "20/06/2024", 7, "Machala", "Quito", 24.00, "Televisión,internet", 2, false, 5.50, "Comida");
 
 
 
 // public Normal(String nombre, String cedula, int edad, String cuidad, String codigo, String fecha_salida, String fecha_llegada, int numero_bus, String lugar_salida, String lugar_llegada, Double precio, String seleccion_asiento, int espacio_maletas, Boolean maleta_extra, Double cargo_extra)
+        Normal cliente6 = new Normal();
         Normal cliente7 = new Normal();
-        ServicioExtra cliente8 = new ServicioExtra();
+        Normal cliente8 = new Normal();
         Normal cliente9 = new Normal();
-        ServicioExtra cliente10 = new ServicioExtra();
-        Normal cliente11 = new Normal();
-        Normal cliente12 = new Normal();
-        Normal cliente13 = new Normal();
+        Normal cliente10 = new Normal();
 
 
-        cliente7.setNombre("Pablo");
+        cliente6.setNombre("Pablo");
+        cliente6.setCedula("1726195207");
+        cliente6.setEdad(19);
+        cliente6.setCuidad("Quito");
+        cliente6.setCodigo("1234");
+        cliente6.setFecha_llegada("14/06/2024");
+        cliente6.setFecha_salida("15/06/2024");
+        cliente6.setNumero_bus(5);
+        cliente6.setLugar_llegada("Cuenca");
+        cliente6.setLugar_salida("Salinas");
+        cliente6.setSeleccion_asiento("ventana");
+        cliente6.setEspacio_maletas(1);
+        cliente6.setMaleta_extra(true);
+        cliente6.setPrecio(5.00);
+
+        cliente7.setNombre("Lupe");
         cliente7.setCedula("1726195207");
         cliente7.setEdad(19);
         cliente7.setCuidad("Quito");
@@ -32,11 +45,28 @@ public class Main {
         cliente7.setFecha_salida("15/06/2024");
         cliente7.setNumero_bus(5);
         cliente7.setLugar_llegada("Cuenca");
-        cliente7.setLugar_salida("Salinas");
+        cliente7.setLugar_salida("Quito");
         cliente7.setSeleccion_asiento("ventana");
         cliente7.setEspacio_maletas(1);
         cliente7.setMaleta_extra(true);
-        cliente7.setPrecio(5.00);
+        cliente7.setPrecio(25.25);
+        cliente7.setCargo_extra(5.00);
+
+        cliente8.setNombre("Andres");
+        cliente8.setCedula("1726195208");
+        cliente8.setEdad(30);
+        cliente8.setCuidad("Guayaquil");
+        cliente8.setCodigo("1235");
+        cliente8.setFecha_llegada("13/06/2024");
+        cliente8.setFecha_salida("16/06/2024");
+        cliente8.setNumero_bus(6);
+        cliente8.setLugar_llegada("Guayaquil");
+        cliente8.setLugar_salida("Quito");
+        cliente8.setSeleccion_asiento("pasillo");
+        cliente8.setEspacio_maletas(1);
+        cliente8.setMaleta_extra(true);
+        cliente8.setPrecio(25.25);
+        cliente8.setCargo_extra(5.00);
 
         cliente9.setNombre("Lupe");
         cliente9.setCedula("1726195207");
@@ -54,64 +84,36 @@ public class Main {
         cliente9.setPrecio(25.25);
         cliente9.setCargo_extra(5.00);
 
-        cliente11.setNombre("Lupe");
-        cliente11.setCedula("1726195207");
-        cliente11.setEdad(19);
-        cliente11.setCuidad("Quito");
-        cliente11.setCodigo("1234");
-        cliente11.setFecha_llegada("14/06/2024");
-        cliente11.setFecha_salida("15/06/2024");
-        cliente11.setNumero_bus(5);
-        cliente11.setLugar_llegada("Cuenca");
-        cliente11.setLugar_salida("Quito");
-        cliente11.setSeleccion_asiento("ventana");
-        cliente11.setEspacio_maletas(1);
-        cliente11.setMaleta_extra(true);
-        cliente11.setPrecio(25.25);
-        cliente11.setCargo_extra(5.00);
-
-        cliente12.setNombre("Andres");
-        cliente12.setCedula("1726195208");
-        cliente12.setEdad(30);
-        cliente12.setCuidad("Guayaquil");
-        cliente12.setCodigo("1235");
-        cliente12.setFecha_llegada("13/06/2024");
-        cliente12.setFecha_salida("16/06/2024");
-        cliente12.setNumero_bus(6);
-        cliente12.setLugar_llegada("Guayaquil");
-        cliente12.setLugar_salida("Quito");
-        cliente12.setSeleccion_asiento("pasillo");
-        cliente12.setEspacio_maletas(1);
-        cliente12.setMaleta_extra(true);
-        cliente12.setPrecio(25.25);
-        cliente12.setCargo_extra(5.00);
-
-        cliente13.setNombre("Pedro");
-        cliente13.setCedula("1726195209");
-        cliente13.setEdad(21);
-        cliente13.setCuidad("Quito");
-        cliente13.setCodigo("1324");
-        cliente13.setFecha_llegada("11/06/2024");
-        cliente13.setFecha_salida("16/06/2024");
-        cliente13.setNumero_bus(5);
-        cliente13.setLugar_llegada("Puyo");
-        cliente13.setLugar_salida("Quito");
-        cliente13.setSeleccion_asiento("ventana");
-        cliente13.setEspacio_maletas(1);
-        cliente13.setMaleta_extra(false);
-        cliente13.setPrecio(28.25);
-        cliente13.setCargo_extra(5.00);
+        cliente10.setNombre("Pedro");
+        cliente10.setCedula("1726195209");
+        cliente10.setEdad(21);
+        cliente10.setCuidad("Quito");
+        cliente10.setCodigo("1324");
+        cliente10.setFecha_llegada("11/06/2024");
+        cliente10.setFecha_salida("16/06/2024");
+        cliente10.setNumero_bus(5);
+        cliente10.setLugar_llegada("Puyo");
+        cliente10.setLugar_salida("Quito");
+        cliente10.setSeleccion_asiento("ventana");
+        cliente10.setEspacio_maletas(1);
+        cliente10.setMaleta_extra(false);
+        cliente10.setPrecio(28.25);
+        cliente10.setCargo_extra(5.00);
 
         cliente1.mostrarDatos();
         cliente1.precio_total();
         System.out.println();
         cliente2.mostrarDatos();
         cliente2.precio_total();
+        System.out.println();
         cliente3.mostrarDatos();
         cliente3.precio_total();
         System.out.println();
         cliente4.mostrarDatos();
         cliente4.precio_total();
+        System.out.println();
+        cliente5.mostrarDatos();
+        cliente5.precio_total();
         System.out.println();
         cliente6.mostrarDatos();
         cliente6.precio_total();
@@ -127,15 +129,6 @@ public class Main {
         System.out.println();
         cliente10.mostrarDatos();
         cliente10.precio_total();
-        System.out.println();
-        cliente11.mostrarDatos();
-        cliente11.precio_total();
-        System.out.println();
-        cliente12.mostrarDatos();
-        cliente12.precio_total();
-        System.out.println();
-        cliente13.mostrarDatos();
-        cliente13.precio_total();
         System.out.println();
     System.out.println("h");
 
